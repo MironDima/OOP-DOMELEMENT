c54a32e809756a3bacbea2b12f2e8b81d78baacf
Miron Dima <mirontsov2098@mail.ru>
2023-03-03 20:30:38 +0300

    add lesson15

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
-console.log('HI');
+
+function DomElement () {
+	this.selector,
+	this.height = '25px',
+	this.width = '25px',
+	this.bg = 'green',
+	this.fontSize = '20px',
+	this.createElem = function (value) {
+
+	if (value.startsWith('.')) {
+		let div = document.createElement('div');
+		div.classList.add('block');
+		div.innerHTML = 'text';
+		div.style.cssText =	`height:${this.height};
+		width:${this.width}; 
+		background:${this.bg};
+		font-size:${this.fontSize}`
+		document.body.append(div);
+		console.log(div);
+	}
+	if (value.startsWith('#')) {
+		let p = document.createElement('p')
+		p.setAttribute('id','block')
+		p.innerHTML = 'text';
+		p.style.cssText =	`height:${this.height};
+		width:${this.width}; 
+		background:${this.bg};
+		font-size:${this.fontSize}`
+		document.body.append(p)
+		console.log(p);
+	}
+	}
+}
+const domElement = new DomElement()
+domElement.createElem('#asd')
+console.log(domElement);
 
