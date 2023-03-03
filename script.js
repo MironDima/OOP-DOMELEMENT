50ba44e93c8438ef36dda87cb529856828e4316b
Miron Dima <mirontsov2098@mail.ru>
2023-03-03 22:17:22 +0300

    add change

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
-function DomElement () {
-	this.selector,
-	this.height = '25px',
-	this.width = '25px',
-	this.bg = 'green',
-	this.fontSize = '20px',
-	this.createElem = function (value) {
+function DomElement (selector, height, width, baground = 'green', fontsize = '32px') {
+	this.selector = selector,
+	this.height = height,
+	this.width = width,
+	this.bg = baground,
+	this.fontSize = fontsize,
 
-	if (value.startsWith('.')) {
+	this.createElem = function (selector) {
+	if (selector.startsWith('.')) {
 		let div = document.createElement('div');
 		div.classList.add('block');
 		div.innerHTML = 'text';
@@ -18,20 +18,25 @@ function DomElement () {
 		document.body.append(div);
 		console.log(div);
 	}
-	if (value.startsWith('#')) {
+	if (selector.startsWith('#')) {
 		let p = document.createElement('p')
 		p.setAttribute('id','block')
 		p.innerHTML = 'text';
-		p.style.cssText =	`height:${this.height};
-		width:${this.width}; 
-		background:${this.bg};
-		font-size:${this.fontSize}`
 		document.body.append(p)
 		console.log(p);
 	}
+
+	this.createSquare = function () {
+		let square =  document.createElement('div')
+		square.style.cssText = "position: absolute; height: 100px; width:100px; background-color: black;"
+		document.body.append(square)
+		console.log(square);
+	}
 	}
 }
-const domElement = new DomElement()
-domElement.createElem('#asd')
+const domElement = new DomElement('.asd','25px','22','35px')
+domElement.createElem('.asd')
+//domElement.createSquare()
 console.log(domElement);
+
 
